6f5e37847eb047ba762e9b4e69d8464eaf3534e0
stefan.pouwels@gmail.com <stefan.pouwels@gmail.com@682a3b78-2556-0410-948b-bbb13db00d4a>
2008-12-07 22:00:19 +0000

    Al wat beter...

---
+++ switch20081207.py
@@ -0,0 +1,33 @@
+import wsgiref.handlers
+import re
+from google.appengine.ext import webapp
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+      
+      target = 'uhm, not found?'
+      
+      if re.search(r'ladyonictprojects.nl', self.request.host):
+        target = open("ladyOnIctProjects/index.html", "r").read()
+      elif re.search(r'zuidvast.nl', self.request.host):
+        target = open("zuidvast/index.html", "r").read()    
+      elif re.search(r'bijna2009.', self.request.host):
+        target = open("bijna2009/index.html", "r").read()   
+      elif re.search(r'localhost.', self.request.host):
+        target = open("bijna2009/index.html", "r").read()   		           
+      else:
+        target = open("index.html", "r").read()   
+   
+      self.response.headers['Content-Type'] = 'text/html'
+      self.response.out.write(target)
+
+def main():
+  application = webapp.WSGIApplication([('/', MainPage),
+                                        ('/signup', Guestbook)],
+									  	debug=True)
+									  
+  wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == "__main__":
+  main()
+  
